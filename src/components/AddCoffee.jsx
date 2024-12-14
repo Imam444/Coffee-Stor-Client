bedaaed3f4bae6b747631d2e0bd02bd652eeaba7
Imam444 <mdimamhossain565528@gmail.com>
2024-12-14 21:03:34 +0600

    send coffee data to the server and store in the mongodb

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,18 +1,48 @@
-const AddCoffee = () => {
-    const handleAddCoffee = event => {
-        event.preventDefault();
-        const form = event.target
+import Swal from "sweetalert2";
 
-        const name = form.name.value;
-        const quantity = form.quantity.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photo  = form.photo .value;
-        
-        const newCoffee = {name,quantity,supplier, taste,category, details, photo }
-    }
+const AddCoffee = () => {
+  const handleAddCoffee = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+
+    console.log(newCoffee);
+
+    fetch("http://localhost:4000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertId) {
+          Swal.fire({
+            title: "Success!",
+            text: "User Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
   return (
     <div className="bg-[#F4F3F0] p-24">
       <h2 className="text-3xl font-extrabold">Add Coffee</h2>
@@ -56,7 +86,7 @@ const AddCoffee = () => {
               <input
                 type="text"
                 placeholder="Supplier Name"
-                name="name"
+                name="supplier"
                 className="input input-bordered w-full"
               />
             </label>
@@ -114,14 +144,18 @@ const AddCoffee = () => {
               <input
                 type="text"
                 placeholder="Photo URL"
-                name="photo "
+                name="photo"
                 className="input input-bordered w-full"
               />
             </label>
           </div>
         </div>
-        
-        <input type="submit" value="Add Coffee" className=" bg-black btn btn-block" />
+
+        <input
+          type="submit"
+          value="Add Coffee"
+          className=" bg-black btn btn-block"
+        />
       </form>
     </div>
   );
